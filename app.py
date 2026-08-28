77a49927a3de649c8f1c41f9242d34a83c952e58
Sheeza-Tanveer <sheezatanveer28@gmail.com>
2026-08-28 07:55:14 -0700

    changes added

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def chat():
     return str(response["answer"])
 
 
-# Run Flask application on port 8080
+# Run the Flask application on port 8080
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
